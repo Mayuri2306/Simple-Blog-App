761f3bab7962d9d6a3d5cc5c93dca0bd055c1b48
Mayuri2306 <mayurijhade104@gmail.com>
2026-08-11 14:20:39 +0530

    creted s3 bucket for tfstate

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -6,7 +6,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "bucket-remote-tfstate"
+    bucket         = "bucket-remote-tfstate-simple-blog-app"
     key            = "amazona-app/terraform.tfstate"
     region         = "us-west-2"
     use_lockfile   =  true
