ca7f213c5ec9a12b4f6035c843b5f192d4c86a0e
Mayuri2306 <mayurijhade104@gmail.com>
2026-08-11 16:58:14 +0530

    fix capacity provider error

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -64,6 +64,8 @@ resource "aws_ecs_service" "service" {
   task_definition = aws_ecs_task_definition.task_def.arn
   desired_count   = 1
 
+  force_new_deployment = true
+
   load_balancer {
     target_group_arn = var.target_group_arn
     container_name   = "backend"
