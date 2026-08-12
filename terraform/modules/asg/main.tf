092095ff08fffc97a6c2ad5be52c275fb33b1caf
Mayuri2306 <mayurijhade104@gmail.com>
2026-08-12 15:34:24 +0530

    fix capacity provider issue

--- terraform/modules/asg/main.tf
+++ terraform/modules/asg/main.tf
@@ -75,7 +75,7 @@ resource "aws_ecs_capacity_provider" "ecs_capacity_provider" {
     managed_scaling {
 
       status                    = "ENABLED"
-      target_capacity           = 60
+      target_capacity           = 80
       minimum_scaling_step_size = 1
       maximum_scaling_step_size = 3
     }
