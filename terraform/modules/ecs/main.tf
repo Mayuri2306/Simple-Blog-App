7f2c8e560cd79c2b28b49297ffe22f3d7013f3e7
Mayuri2306 <mayurijhade104@gmail.com>
2026-08-12 15:30:29 +0530

    reduce memory and cpu allocation

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -8,8 +8,8 @@ resource "aws_ecs_task_definition" "task_def" {
   requires_compatibilities = ["EC2"]
   network_mode             = "bridge"
 
-  cpu    = "256"
-  memory = "512"
+  cpu    = "128"
+  memory = "256"
 
   execution_role_arn = aws_iam_role.ecs_execution_role.arn
   task_role_arn      = aws_iam_role.ecs_task_role.arn
